636552d95ed30bf3a6ef98dc177763684c95c99e
Kamila Nowak <47334332+KamilaNowak@users.noreply.github.com>
2019-12-07 19:46:00 +0100

    Add files via upload

--- Wypozyczenie.cpp
+++ Wypozyczenie.cpp
@@ -1,3 +1,52 @@
 #include <iostream>
 #include "Wypozyczenie.hpp"
 using namespace std;
+
+
+bool Wypozyczenie::wypozyczAuto(Pojazd* pojazd,Uzytkownik* u, Data dataP, Data dataK) {
+	Wypozyczenie * wypozyczenie = new Wypozyczenie();
+	wypozyczenie->setAktualne(false);
+	wypozyczenie->setDataKoncowa(dataK);
+	wypozyczenie->setDataPoczatkowa(dataP);
+	wypozyczenie->setPojazd(pojazd);
+	wypozyczenie->setUzytkownik(uzytkownik);
+	return true;
+}
+
+void Wypozyczenie::zwroc() {
+	//zrobic
+}
+void Wypozyczenie::wyswietlWarunki() {
+	//TODO
+}
+
+Pojazd* Wypozyczenie::getPojazd() {
+	return pojazd;
+}
+Uzytkownik* Wypozyczenie::getUzytkownik() {
+	return uzytkownik;
+}
+bool Wypozyczenie::getAktualne() {
+	return aktualne;
+}
+void Wypozyczenie::setAktualne(bool b) {
+	aktualne = b;
+}
+Data Wypozyczenie::getDataKoncowa() {
+	return dataKoncowa;
+}
+Data Wypozyczenie::getDataPoczatkowa() {
+	return dataPoczatkowa;
+}
+void Wypozyczenie::setDataKoncowa(Data data) {
+	dataKoncowa = data;
+}
+void Wypozyczenie::setDataPoczatkowa(Data data) {
+	dataPoczatkowa = data;
+}
+void Wypozyczenie::setPojazd(Pojazd *pojazd) {
+	pojazd = pojazd;
+}
+void Wypozyczenie::setUzytkownik(Uzytkownik* u) {
+	uzytkownik = u;
+}
--- Wypozyczenie.hpp
+++ Wypozyczenie.hpp
@@ -3,17 +3,28 @@
 
 #include "Pojazd.hpp"
 #include "Data.hpp"
-class Wypozyczenie{
+#include "Uzytkownik.hpp"
+class Wypozyczenie {
 private:
 	Data dataPoczatkowa;
 	Data dataKoncowa;
 	bool aktualne;
 	Pojazd* pojazd;
+	Uzytkownik* uzytkownik;
 public:
-	bool wypozyczAuto(Pojazd *pojazd);
-	bool wypozyczMotocykl();
+	bool wypozyczAuto(Pojazd *pojazd, Uzytkownik* u,Data datak, Data dataP);
 	void zwroc();
 	void wyswietlWarunki();
+	Data getDataKoncowa();
+	Data getDataPoczatkowa();
+	void setDataKoncowa(Data);
+	void setDataPoczatkowa(Data);
+	bool getAktualne();
+	void setAktualne(bool);
+	Pojazd* getPojazd();
+	void setPojazd(Pojazd *pojazd);
+	Uzytkownik* getUzytkownik();
+	void setUzytkownik(Uzytkownik* u);
 };
 #endif
 
