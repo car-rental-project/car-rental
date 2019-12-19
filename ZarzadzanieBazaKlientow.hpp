edf93800e60f72c43310b52ca1b53266fa8872ca
Camillo-commits <58623609+Camillo-commits@users.noreply.github.com>
2019-12-19 08:45:23 +0100

    Create ZarzadzanieBazaKlientow.hpp

---
+++ ZarzadzanieBazaKlientow.hpp
@@ -0,0 +1,13 @@
+#ifndef ZARZADZANIEBAZAKLIENTOW
+#define ZARZADZANIEBAZAKLIENTOW
+
+
+class ZarzadzanieBazaKlientow {
+public:
+	void przegladajBazeKlientow();
+	void edytujBazeKlientow();
+	void dodajKlienta();
+	void usunKlienta();
+};
+
+#endif // !ZARZADZANIEBAZAKLIENTOW
